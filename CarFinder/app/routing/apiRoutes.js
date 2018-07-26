ffb97bd07e4d023aa8ae676f37290139e23451d9
muon012 <eduardessc0@hotmail.com>
2018-07-26 16:41:02 -0700

    carfinder finished

--- CarFinder/app/routing/apiRoutes.js
+++ CarFinder/app/routing/apiRoutes.js
@@ -1,17 +1,61 @@
 var express = require("express");
 var objs = require("../data/cars");
 
-var router1 = express.Router();
+var router = express.Router();
 
-router1.get("/cars", function (req, res) {
+router.get("/cars", function (req, res) {
     res.json(objs.cars);
 });
 
-router1.get("/users", function (req, res) {
+router.get("/users", function (req, res) {
     res.json(objs.users);
+});
+
+router.post("/cars", function(req, res){
+
+    var user = req.body.name;
+    var userScore = req.body.score;
+    var diffTotal; // this will be the total difference between a car's score and the user'score.
+    var diffTotalArray = []; // we will place the total diffrences in this array.
+    var carSelected = {
+        car: "",
+        picture: "",
+        score: []
+    }
+
+
+    for(var i= 0; i < objs.cars.length; i++){
+        var carScore = objs.cars[i].score;
+        // let "diffTotal = 0" because the total difference amongst each car and the user must be reseted to 0 for each different car, otherwise, it will add the previous result to the current result.
+        diffTotal = 0;
+
+        for(var j= 0; j < carScore.length; j++){
+            var diffDigit = Math.abs(carScore[j]-userScore[j]); // find the absolute value of the difference of each number in the car and user arrays.
+            diffTotal += diffDigit; // add all the absolute values to "0".
+        }
+
+        diffTotalArray.push(diffTotal); // the diferrence for each car is placed in the array.
+        var lowestDiff = Math.min.apply(null,diffTotalArray); // we find the lowest value in such array.
+        // console.log(diffTotalArray);
+        // console.log(lowestDiff);
+        var carForUser = diffTotalArray.indexOf(lowestDiff); // find the index of the lowest difference so we can use such number to find the car from the cars' array.
+        // console.log(carForUser);
+        carSelected.car = objs.cars[carForUser].car;
+        carSelected.picture = objs.cars[carForUser].picture;
+    }
+
+    objs.users.push(
+        {
+            name: user,
+            score: userScore,
+            car: carSelected.car
+        });
+    
+    res.json(carSelected);
+
 });
 
 // In the file "cars.js" an onject was created using module.exports. We are accessing that object by "require" and then set it
 // equal to "var objs". We then use the specific property of the object we want to address like "objs.cars" and "objs.users".
 
-module.exports= router1;
+module.exports= router;
--- CarFinder/server.js
+++ CarFinder/server.js
@@ -10,12 +10,13 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+// using the routes exported from "apiRoutes.js" and "htmlRoutes.js"
 app.use("/", routes);
 app.use("/api", routes2);
-// app.get("/api/cars", function(req,res){
-//     res.send(routes1);
-// })
 
+
+// sending the static folder "public" so it could be referenced in "home.html" and "survey.html" for css purposes.
 app.use(express.static("app/public"));
 
 
